17de7fb1de4206b6fca1f85a8115e6da258672e4
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-25 10:59:45 +0600

    data created

--- index.js
+++ index.js
@@ -1,64 +1,112 @@
-const express = require("express");
-const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-//middleware
+const categories = require("./data/category.json")
+const news = require("./data/product.json")
+
+// middle ware
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-//mongo db
-const uri = `mongodb+srv://${process.env.ADMIN_USER}:${process.env.ADMIN_PASSWORD}@cluster0.guw4vbk.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+app.get("/", (req, res) => {
+    res.send("product server running")
+})
+
+app.get("/brands", (req, res) => {
+    res.send(categories);
 });
 
-// crud operations
+
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MONGO DB
+
+const uri = `mongodb+srv://${process.env.ADMIN_USER}:${process.env.ADMIN_PASS}@cluster0.guw4vbk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 async function run() {
+
     try {
-        const serviceCollection = client.db("laptopHub").collection("category");
+
+        const brandCollection = client.db("usedProduct").collection("brand");
+
+        const productCollection = client.db("usedProduct").collection("products")
 
 
 
         // get api
-        app.get("/services", async (req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        });
-        app.get("/services/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
 
+        // app.get("/brands", async (req, res) => {
+        //     const query = {};
+        //     const brands = await brandCollection.find(query).toArray()
+        //     res.send(brands)
+        // })
 
-
+        // app.get('/category/:id', (req, res) => {
+        //     const id = req.params.id;
+        //     const productCategory = product.filter((product) => product.category_id === id);
+        //     res.send(productCategory)
+        // })
 
 
     }
+
+
+
+
+
 
 
     finally {
 
-
     }
+
 }
-run().catch((err) => console.log(err));
-app.get("/", (req, res) => {
-    res.send("product server running");
-});
 
-app.listen(port, (req, res) => {
-    console.log(`server running ${port}`);
-});
+run().catch(err => console.log(err))
 
 
 
@@ -66,6 +114,10 @@ app.listen(port, (req, res) => {
 
 
 
+// QECfFi55pmCHzvnS
+// usedProduct
 
 
-
+app.listen(port, () => {
+    console.log("server running", port);
+})
